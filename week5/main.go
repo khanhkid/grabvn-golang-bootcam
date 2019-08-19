ce7cf4a1f797b11be206d96ca0ba3022cc936f7e
khanhvo <khanhvo@vinaresearch.net>
2019-08-19 17:50:18 +0700

    update week5 assignment

---
+++ week5/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"./communicate"
+)
+
+//TODO: how to separate API logic, business logic and response format logic
+func main() {
+	fmt.Print("\tPlease select type:\n1: JSON\n2: XML\n\nInput: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	var isJSON bool
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "1" {
+			isJSON = true
+			break
+		}
+		if text == "2" {
+			isJSON = false
+			break
+		}
+
+		fmt.Print("\nPlease input 1 or 2")
+		fmt.Print("\nInput: ")
+	}
+
+	http.HandleFunc("/postWithComments", communicate.ChooseType(isJSON))
+
+	log.Println("httpServer starts ListenAndServe at 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
